bbdcf808dcfc6e4ff738a707b95a0f414524abba
rafat86 <115078051+rafat86@users.noreply.github.com>
2022-10-30 16:59:21 +0200

    4th commit

--- main.py
+++ main.py
@@ -19,30 +19,63 @@ class Date:
         order = order + self.day
         return(order)
 
-    def print_info(self):
-        print("your Date is ", str(self.day), "/", str(self.month), "/", str(self.year))
+    def __str__(self):
+        return str(self.day) + "/" + str(self.month) + "/" + str(self.year)
 
     def __add__(self, other):
         self.day = self.day + other
-        if self.month in self.months_28days:
-            if self.day > 28:
-                self.day = self.day - 28
-                self.month = self.month + 1
-        elif self.month in self.months_30days:
-            if self.day > 30:
-                self.day = self.day - 30
-                self.month = self.month + 1
-                if self.month > 12:
-                    self.month = 1
-                    self.year = self.year + 1
-        else:
-            if self.day > 30:
-                self.day = self.day - 31
-                self.month = self.month + 1
-        print(self.day, self.month, self.year)
+        while self.day > 28:
+            if self.month in self.months_28days:
+                if self.day > 28:
+                    self.day = self.day - 28
+                    self.month = self.month + 1
+            elif self.month in self.months_30days:
+                if self.day > 30:
+                    self.day = self.day - 30
+                    self.month = self.month + 1
+                    if self.month > 12:
+                        self.month = 1
+                        self.year = self.year + 1
+            else:
+                if self.day > 31:
+                    self.day = self.day - 31
+                    self.month = self.month + 1
+        print(self)
 
-d1 = Date(9, 12, 2000)  # d1 = date(input("enter the date: ").split(","))
-d1.print_info()
+    def __sub__(self, other):
+        return abs(self.order() - other.order())
+
+    def __lt__(self, other):
+        return self.order() < other
+
+    def __gt__(self, other):
+        return self.order() > other
+
+    def __le__(self, other):
+        return self.order() <= other
+
+    def __ge__(self, other):
+        return self.order() >= other
+
+    def __eq__(self, other):
+        return self.order() == other
+
+    def __ne__(self, other):
+        return self.order() != other
+
+d, m, y = input("Enter a date :   ").split(",")
+d1 = Date(int(d), int(m), int(y))
+print(d1)
+
 print("The order of the day in the year is: ", d1.order(), "th")
-d2 = d1 + 30
+added_days = int(input("Enter number of days to add:"))
+d2 = d1 + added_days
+print(d2)
 
+compare_tonumber=int(input("Enter anumber to campare:    "))
+print ("is", d1, "<", compare_tonumber,d1 < compare_tonumber )
+
+d_sub, m_sub, y_sub = input("Enter a date :   ").split(",")
+d4 = Date(int(d_sub), int(m_sub), int(y_sub))
+d3 = d1 - d4
+print(d3)

70b1312fc3c646264ea2962ad44b6b7d4d5975c1
rafat86 <115078051+rafat86@users.noreply.github.com>
2022-10-30 09:57:41 +0200

    2nd commit

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ class date:
         self.day = day
         self.month = month
         self.year = year
+        self.monthts_28days = {2}
+        self.monhths_30days = {4, 6, 9, 12}
+        self.months_31days = {1, 3, 5, 7, 8, 10, 11}
     def print_info(self):
         print(str(self.day), str(self.month), str(self.year))
 
-    def __order__(self):
+    def __order__
 
-
-#d1= date(7, 8, 1986)
+#d1 = date(input("enter the date: ").split(","))
+d1 = date(7, 8, 1986)
 d1.print_info()
 
